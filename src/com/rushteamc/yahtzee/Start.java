d63962faec7947755a1766970015202d9ce6ebbf
Eivind Vegsundvaag <eivindveg@hotmail.com>
2013-09-13 00:34:58 +0200

    Maybe Runnetty wants to read this now.

--- src/com/rushteamc/yahtzee/Start.java
+++ src/com/rushteamc/yahtzee/Start.java
@@ -7,32 +7,41 @@ public class Start
 
 	public static void main(String[] args)
 	{
-                PlayerTurnCheck.TotalPlayingPlayers(); // Will Output Total Players
-                PlayerTurnCheck.PrintPlayerTurn();// Just Prints Curent Players Turn and game Round
-                PlayerTurnCheck.NextTurn();// Next Players Turn :D
+		/*
+		 * Dummy main method for debugging until we start implementing functions!
+		 */
+                PlayerTurnCheck.TotalPlayingPlayers();	// Will Output Total Players
+                PlayerTurnCheck.PrintPlayerTurn();		// Just Prints Curent Players Turn and game Round
+                PlayerTurnCheck.NextTurn();				// Next Players Turn :D
                 
-		int[] Dice = com.rushteamc.yahtzee.utils.RollDice.castDice();
-		int[] Values = com.rushteamc.yahtzee.utils.DieCounter.countValues(Dice);
-		boolean[] Numbers = com.rushteamc.yahtzee.utils.DieCounter.truthValues(Values);
-		int Score = 0;
-		int ScoreUpper = 0;
+		int[] Dice = com.rushteamc.yahtzee.utils.RollDice.castDice(); 						// Init and populate Dice array with random numbers.
+		int[] Values = com.rushteamc.yahtzee.utils.DieCounter.countValues(Dice); 			// Turn the useless dice array into useful values.
+		boolean[] Numbers = com.rushteamc.yahtzee.utils.DieCounter.truthValues(Values);		// Create booleans for whether or not a number is present.
+		int Score = 0;		// Preinit.
+		int ScoreUpper = 0;	// Preinit.
 		for
 		(
 			int i =1;
-				i <= com.rushteamc.yahtzee.utils.Configurables.getDieSize();
+				i <= com.rushteamc.yahtzee.utils.Configurables.getDieSize();				// For every face on the dice:
 				i++
 		)
 		{
-			ScoreUpper += com.rushteamc.yahtzee.utils.ScoreTestsUpper.checkUpper(Dice, i);
+			ScoreUpper += com.rushteamc.yahtzee.utils.ScoreTestsUpper.checkUpper(Dice, i);	// Multiply values with amounts and get points! :D
 		}
-		Score += ScoreUpper;
-		Score += com.rushteamc.yahtzee.utils.ScoreTestsUpper.checkForBonus(ScoreUpper);
+		Score += ScoreUpper;	// Add ScoreUpper to total score.
+		Score += com.rushteamc.yahtzee.utils.ScoreTestsUpper.checkForBonus(ScoreUpper);		// Check if ScoreUpper qualifies for bonus points!
+		/* 
+		 * Self explanatory below.
+		 */
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForThreeOfAKind(Values);
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForFourOfAKind(Values);
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForFullHouse(Values);
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForSmallStraight(Numbers);
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForLargeStraight(Numbers);
 		Score += com.rushteamc.yahtzee.utils.ScoreTestsLower.checkForChance(Values);
+		/*
+		 *  End self-explanatory. Start debug.
+		 */
 		debugDie(Dice, Values);
 		
 		System.out.println("Total score: " + Score);
