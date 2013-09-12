46c8a7f5c95e528d2da7cc7b8e288d4442b446ce
Runnetty1 <rcdriftnitro2@gmail.com>
2013-09-12 23:18:22 +0200

    Shortened and Simplified PlayerTurn Check
    Also generated a test build folder >.<

--- src/com/rushteamc/yahtzee/utils/PlayerTurnCheck.java
+++ src/com/rushteamc/yahtzee/utils/PlayerTurnCheck.java
@@ -8,56 +8,40 @@ public class PlayerTurnCheck {
     public static String[] Players = new String[NrPlyrs];
     
     public static String Curplayer = ""; //Will Display
-    public static int Turn = 0;
+    public static int Turn = 1;
     public static int Round = 1;
     
     public static void CheckForPlayerTurn()
     {   
         
-        for(int MaxTurns = NrPlyrs; Turn < MaxTurns;)
+        /*Checks Curent Turn is greater than the number of players*/
+        /////////////////////////////////////////////////////////////
+            for(int MaxTurns = NrPlyrs; Turn > MaxTurns;)
         {
-            PrintPlayerTurn();
+            System.out.println("The current round is finished");
+            Round++;
+            Turn=1;
         }
-        
-        
-        
-        
-        /*Checks Curent Turn*/
-        /* - Trying to Easyfy this
-        ////////////////////////////////
-        if(Turn == 1)
-        {
-             PrintPlayerTurn();
-        }
-        if(Turn == 2)
-        {
-            PrintPlayerTurn(); 
-        }
-        if(Turn == 3)
-        {
-           PrintPlayerTurn();
-        }      
-        if(Turn == 4)
-        {
-           PrintPlayerTurn();
-          Round++;
-          Turn = 0;
-        }
-        //////////////////////////////////
-        */
+        /////////////////////////////////////////////////////////////
     }
     
     public static void PrintPlayerTurn()
     {
         Curplayer = "Its Player " + Turn + "'s Turn"+" on round: " + Round;
-        System.out.println(Curplayer);   
-        
+        System.out.println(Curplayer); 
+    }
+    
+     public static void TotalPlayingPlayers()
+             
+    {
+        System.out.println("There are: "+Players.length+ " players");   
     }
     
      public static void NextTurn()
     {
-        Turn++; //Adds 1 to Current Turn
-        
+        Turn++; //Adds 1 to Current Turn 
+        CheckForPlayerTurn();
+        PrintPlayerTurn();
     }
 
 }
