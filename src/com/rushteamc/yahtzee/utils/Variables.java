64072b092929a749287c8e5f6065759873003f1e
Runnetty1 <rcdriftnitro2@gmail.com>
2013-09-17 16:54:13 +0200

    Created Class for all Variables
    Migration of public Variables are to go here.

---
+++ src/com/rushteamc/yahtzee/utils/Variables.java
@@ -0,0 +1,32 @@
+
+package com.rushteamc.yahtzee.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Runnetty
+ */
+public class Variables {
+    
+        /////////// - I N T - ////////////
+            /*PlayerTurnCheck.class*/
+                public static int Turn = 1;
+                public static int Round = 1;    
+
+        /////////// - S T R I N G - ////////////
+            /*PlayerTurnCheck.class*/
+                public static String Curplayer; // Will Output Current Player on round
+                public static String TotalPlayers; // Will Output Total Players 
+                public static String AllPlayers;
+        /////////// - B O O L E A N - ////////////
+        
+        /////////// - F L O A T - ////////////
+        
+        /////////// - A R R A Y S - ////////////
+            /*Players.class*/    
+                public static List PlayerNames = new ArrayList<>(); //Creates a player list array.
+        
+
+}
