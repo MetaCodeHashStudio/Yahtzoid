6c2d808c4cd8335bdbbe3f6f52fd199fbaefe4b2
RushTea <eivindveg@hotmail.com>
2013-09-20 22:26:29 +0200

    Properly conform to java naming conventions

--- src/com/rushteamc/yahtzee/utils/Variables.java
+++ src/com/rushteamc/yahtzee/utils/Variables.java
@@ -12,21 +12,21 @@ public class Variables {
     
         /////////// - I N T - ////////////
             /*PlayerTurnCheck.class*/
-                public static int Turn = 1;
-                public static int Round = 1;    
+                public static int turnNumber = 1;
+                public static int roundNumber = 1;    
 
         /////////// - S T R I N G - ////////////
             /*PlayerTurnCheck.class*/
-                public static String Curplayer; // Will Output Current Player on round
-                public static String TotalPlayers; // Will Output Total Players 
-                public static String AllPlayers;
+                public static String curPlayer; // Will Output Current Player on round
+                public static String totalPlayers; // Will Output Total Players 
+                public static String allPlayers;
         /////////// - B O O L E A N - ////////////
         
         /////////// - F L O A T - ////////////
         
         /////////// - A R R A Y S - ////////////
             /*Players.class*/    
-                public static List PlayerNames = new ArrayList<>(); //Creates a player list array.
+                public static List playerNames = new ArrayList<>(); //Creates a player list array.
         
 
 }
