3ccf4005284168a25cf5241fb00236d26e91a577
RushTea <eivindveg@hotmail.com>
2013-09-20 22:25:08 +0200

    Properly conform to java naming conventions.

--- src/com/rushteamc/yahtzee/utils/Players.java
+++ src/com/rushteamc/yahtzee/utils/Players.java
@@ -13,7 +13,7 @@ public class Players {
     // To get a speciffic player name from the list, type:
     // Variables.PlayerNames.get(<the player nr id>)
     
-    public static void CreatePlayers(){ //Adds names to the list.
+    public static void createPlayers(){ //Adds names to the list.
         
         /*Test adds*/
         Variables.PlayerNames.add("Runnetty");
@@ -25,7 +25,7 @@ public class Players {
         /*Needs to get string from GUI input field*/
     }
     
-    public static void GetAllPlayerNames() //Gets all names and puts in string called AllPlayers.
+    public static void getAllPlayerNames() //Gets all names and puts in string called AllPlayers.
     { 
         int Nrplayrs = Variables.PlayerNames.size();
         
@@ -40,17 +40,18 @@ public class Players {
        
     }
     
-    public static void DeleteAllPlayerNames() //Self Explainatory.
+    public static void deleteAllPlayerNames() //Self Explainatory.
     {
         Variables.PlayerNames.removeAll(Variables.PlayerNames.subList(0, Variables.PlayerNames.size()));
         
         if(!Variables.PlayerNames.isEmpty()) //Checks if the list is empty, if true print error.
         {
             System.out.println("Theres no player names in the list to delete");
-        }else
-         {
-             System.out.println("All Players Deleted");
-         }
+        }
+        else
+        {
+        	System.out.println("All Players Deleted");
+        }
         
     }
     
