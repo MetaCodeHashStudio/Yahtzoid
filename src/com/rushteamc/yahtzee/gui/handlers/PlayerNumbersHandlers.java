f67fe51fe1d6806e1476ce5b03e93d5ea8c03262
RushTea <eivindveg@hotmail.com>
2013-09-24 23:00:45 +0200

    Indentations "fixed"

--- src/com/rushteamc/yahtzee/gui/handlers/PlayerNumbersHandlers.java
+++ src/com/rushteamc/yahtzee/gui/handlers/PlayerNumbersHandlers.java
@@ -12,27 +12,28 @@ public class PlayerNumbersHandlers
 	{
 		PlayerNumbers.nextButton.addActionListener
 		(
-				new ActionListener()
+			new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent arg0)
 				{
-					@Override
-					public void actionPerformed(ActionEvent arg0)
-					{
-						String numPlayers = com.rushteamc.yahtzee.gui.utils.GameSetupStates.getSelectedButtonText(PlayerNumbers.numPlayersGroup);
-						Variables.selectedNumPlayers = numPlayers.charAt(0) - 48; // Why the hell do I need to subtract 48?!
-						
-						com.rushteamc.yahtzee.Start.PlayerNumberFrame.setVisible(false);
-						com.rushteamc.yahtzee.Start.continueToNames();
-					}
+					String numPlayers = com.rushteamc.yahtzee.gui.utils.GameSetupStates.getSelectedButtonText(PlayerNumbers.numPlayersGroup);
+					Variables.selectedNumPlayers = numPlayers.charAt(0) - 48; // Why the hell do I need to subtract 48?!
+					com.rushteamc.yahtzee.Start.PlayerNumberFrame.setVisible(false);
+					com.rushteamc.yahtzee.Start.continueToNames();
 				}
+			}
 		);
-                PlayerNumbers.quitButton.addActionListener
-                        (new ActionListener()
-                            {
-                                public void actionPerformed(ActionEvent e)
-                                { 
-                                    System.exit(0);
-                                } 
-                            } 
-                        );
+        PlayerNumbers.quitButton.addActionListener
+        (
+        	new ActionListener()
+        	{
+        		@Override
+       			public void actionPerformed(ActionEvent e)
+        		{ 
+        			System.exit(0);
+        		} 
+        	} 
+        );
 	}
 }
