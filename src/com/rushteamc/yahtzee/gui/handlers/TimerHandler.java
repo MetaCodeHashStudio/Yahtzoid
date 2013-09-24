722b43faa9c5fbbc20d26222c81d1e623b2475f8
Runnetty1 <rcdriftnitro2@gmail.com>
2013-09-24 22:57:30 +0200

    TimerHandler added

---
+++ src/com/rushteamc/yahtzee/gui/handlers/TimerHandler.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rushteamc.yahtzee.gui.handlers;
+
+/**
+ *
+ * @author Runnetty
+ */
+public class TimerHandler {
+    
+}
