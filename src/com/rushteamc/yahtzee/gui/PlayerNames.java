98b1624c2377b1f580a5a1b19fcbaea6fbaa6a17
RushTea <eivindveg@hotmail.com>
2013-09-24 22:48:59 +0200

    Rudamentary initial commit.

--- src/com/rushteamc/yahtzee/gui/PlayerNames.java
+++ src/com/rushteamc/yahtzee/gui/PlayerNames.java
@@ -1,55 +1,69 @@
 package com.rushteamc.yahtzee.gui;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.EventQueue;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class PlayerNames extends JDialog {
+public class PlayerNames extends JFrame {
 
-	private final JPanel contentPanel = new JPanel();
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main() {
-		try {
-			PlayerNames dialog = new PlayerNames();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private JPanel contentPane;
+	
+	
+	public PlayerNumbers()
+	{
+		setTitle("Project Yahtzoid version -1");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 540, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		buttonPane = new JPanel();
+//		mainPane = new JPanel();
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{90,90,90,90,90,90};
+		gbl_contentPane.rowHeights = new int[]{60,60,60,60,60};
+		contentPane.setLayout(gbl_contentPane);
+		setComponent();
+		com.rushteamc.yahtzee.Start.setPlayerNameHandler();
 	}
-
-	/**
-	 * Create the dialog.
-	 */
-	public PlayerNames() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+	public void setComponent()
+	{
+		createButtonPane();
+		createInputs();
+		createNextButton();
+		createQuitButton();
 	}
+	static void createButtonPane()
+	{
+		buttonPane = new JPanel();
+		GridBagLayout gbl_buttonPane = new GridBagLayout();
+		gbl_buttonPane.columnWidths = new int[]{90,90,90,90,90,90};
+		gbl_buttonPane.rowHeights = new int[]{0,40,0};
+		GridBagConstraints gbc_buttonPane = new GridBagConstraints();
+		gbc_buttonPane.fill = GridBagConstraints.BOTH;
+		gbc_buttonPane.gridx = 0;
+		gbc_buttonPane.gridy = 3;
+		gbc_buttonPane.gridwidth = 6;
+		buttonPane.setLayout(gbl_buttonPane);
+		contentPane.add(buttonPane, gbc_buttonPane);
+	}
+	static void createNextButton()
+	{
+		nextButton = new JButton("Next");
+		GridBagConstraints gbc_nextButton = new GridBagConstraints();
+		gbc_nextButton.gridx = 2;
+		gbc_nextButton.gridy = 0;
+		buttonPane.add(nextButton, gbc_nextButton);
+	}
+	static void createQuitButton()
+	{
+		quitButton = new JButton("Quit");
+		buttonPane.add(quitButton);
+	}
+	static void createNameInputs;
 
 }
