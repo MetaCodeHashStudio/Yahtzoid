3ffee4ad79ff3847b45606852c6c21772da473cd
Runnetty1 <rcdriftnitro2@gmail.com>
2013-10-10 01:05:05 +0200

    SaveGameStates!

--- src/com/rushteamc/yahtzee/utils/FileHandling.java
+++ src/com/rushteamc/yahtzee/utils/FileHandling.java
@@ -1,6 +1,9 @@
 package com.rushteamc.yahtzee.utils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 
 public class FileHandling
@@ -22,4 +25,37 @@ public class FileHandling
 		if(isCompiled)
 			Variables.internalFilePath = "jar:file:" + tempPath + "!/"; 
 	}
+        public static void SaveGameState()
+        {
+            PrintStream out = null;
+            boolean success;
+            
+            String Name = System.getProperty("user.name"); 
+            success = (new File("C:\\Users\\" + Name + "\\AppData\\Roaming\\.yahtzoid\\aaa")).mkdirs();
+            if (!success) {
+                System.out.println("Shit! couldnt create folders!");
+            }
+        
+            try
+            {
+                File newTextFile = new File("C:\\Users\\" + Name + "\\AppData\\"
+                        + "Roaming\\.yahtzoid\\aaa\\gameSave.sav");
+                
+                FileWriter fw = new FileWriter(newTextFile);
+                
+                //Insert data into file
+                for(int i = Variables.playerNames.size()-1; i >0; i--){
+                    fw.write(Variables.playerNames.get(i) + ":" + ""); //Each Line populated: Playername:aceScore:twoScore ...etc... etc...
+                }
+                fw.close();   
+            }
+            catch (IOException iox) { 
+                iox.printStackTrace();
+            } 
+            finally {
+                if (out != null) out.close();
+            }
+    }
+            
 }
+
